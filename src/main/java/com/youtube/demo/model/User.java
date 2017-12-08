30e57363d1149aed86efdc47ed699c8d74533d02
Felix Laura <felixlaura@Felixs-MacBook-Pro.local>
2017-12-07 21:09:40 -0500

    Method saveOrUpdate is working

--- src/main/java/com/youtube/demo/model/User.java
+++ src/main/java/com/youtube/demo/model/User.java
@@ -7,65 +7,76 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 @Access(AccessType.FIELD)
 public class User extends ParentEntity {
 
 	private static final long serialVersionUID = 1L;
-	
-	@Column(name="first_name", nullable=false, length= 255)
+
+	@Column(name = "first_name", nullable = false, length = 255)
 	private String firstName;
-	
-	@Column(name="second_name", nullable=true, length= 255)
+
+	@Column(name = "second_name", nullable = true, length = 255)
 	private String secondName;
-	
-	@Column(name="first_surname", nullable=false, length= 255)
+
+	@Column(name = "first_surname", nullable = false, length = 255)
 	private String firstSurname;
-	
-	@Column(name="second_surname", nullable=true, length= 255)
+
+	@Column(name = "second_surname", nullable = true, length = 255)
 	private String secondSurname;
-	
-	@Column(name="phone", nullable=true, length= 30)
+
+	@Column(name = "phone", nullable = true, length = 30)
 	private String phone;
-	
-	@Column(name="address", nullable=false, length= 255)
+
+	@Column(name = "address", nullable = false, length = 150)
 	private String address;
-	
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getSecondName() {
 		return secondName;
 	}
+
 	public void setSecondName(String secondName) {
 		this.secondName = secondName;
 	}
+
 	public String getFirstSurname() {
 		return firstSurname;
 	}
+
 	public void setFirstSurname(String firstSurname) {
 		this.firstSurname = firstSurname;
 	}
+
 	public String getSecondSurname() {
 		return secondSurname;
 	}
+
 	public void setSecondSurname(String secondSurname) {
 		this.secondSurname = secondSurname;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	
+
 }
